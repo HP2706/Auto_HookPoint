a12088f9aa6bfd804d3fb1fb939c8ff1502ef1fa
HP2706 <hprjdk@gmail.com>
2024-07-17 11:28:40 +0200

    change name back

--- Auto_HookPoint/utils.py
+++ Auto_HookPoint/utils.py
@@ -1,8 +1,5 @@
 from typing import Iterable, Tuple, Union
 from torch import nn
-from typing import Callable, List
-import inspect
-from tests import test_auto_hook
 
 def iterate_module(
     module : Union[nn.ModuleList, nn.Sequential, nn.ModuleDict]
